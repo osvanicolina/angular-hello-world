ff682af0ae536b2689187f8f737698c09fca3f3e
Osvani Colina <osvacolijim@gmail.com>
2019-04-22 09:04:41 -0400

    Done initial commit

---
+++ src/app/personas/personas.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-personas',
+    templateUrl: './personas.component.html',
+    styleUrls: ['./personas.component.css']
+})
+export class PersonasComponent{
+    agregarPersona = false;
+    agregarPersonaStatus = "No se ha agregado ninguna persona";
+    tituloPersona = "Ingeniero";
+    personaCreada = false;
+
+    constructor(){
+        setTimeout(
+            () => {
+                this.agregarPersona = true;
+            }
+            , 3000)
+    }
+
+    onCrearPersona(){
+        this.agregarPersonaStatus = "Persona agregada";
+        this.personaCreada = true;
+    }
+
+    onModificarPersona(event: Event){
+        this.tituloPersona = (<HTMLInputElement>event.target).value;
+    }
+}
